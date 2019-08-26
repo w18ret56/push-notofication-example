dfacf869ac1ca6cc312bfbccc96c8571fd3f68d9
w18ret56 <54064436+w18ret56@users.noreply.github.com>
2019-08-26 11:03:54 +0200

    Update back.php

--- back.php
+++ back.php
@@ -1,6 +1,14 @@
 <?php
-$news = file("news.txt");
-$randIndex = array_rand($news);
-$r = explode(";",$news[$randIndex]);
-print json_encode($r);
+$news = Array();
+$news[0]["title"] = "Burza nad Warszawą";
+$news[0]["content"] = "Wczoraj nad Warszawą rozpętała się straszna nawałnica";
+$news[1]["title"] = "Loteria tygodnia";
+$news[1]["content"] = "Wygraj 100 000 lub samochód";
+$news[2]["title"] = "Miejski budżet";
+$news[2]["content"] = "Trwa głosowanie co do miejskiego budżetu";
+$news[3]["title"] = "Mały bohater";
+$news[3]["content"] = "13-letni Tymek z Krakowa uratował swoich rodziców dzwoniąc pod 112";
+$news[4]["title"] = "Problemy ze śmieciami";
+$news[4]["content"] = "Co 4 osoba w Polsce pali śmieciami";
+print json_encode($news);
 ?>
