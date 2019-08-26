135898b9e0baea774d4126283785a3abe50ba4d2
w18ret56 <54064436+w18ret56@users.noreply.github.com>
2019-08-26 10:50:44 +0200

    Update back.php

--- back.php
+++ back.php
@@ -1,6 +1,8 @@
 <?php
 $news = file("news.txt");
 $randIndex = array_rand($news);
-$r = explode($news[$randIndex],";");
-print_r($r);
+print "<br />===<br />";
+print $news[$randIndex]
+print "<br />===<br/>"
+
 ?>
