f116fcc1d37ba6fca02efd433e2363e784c6d1ad
w18ret56 <54064436+w18ret56@users.noreply.github.com>
2019-08-26 11:33:55 +0200

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
-importScript("jquery-1.12.4.min.js");
+importScripts("jquery-1.12.4.min.js");
 $.getJSON("https://vast-waters-68504.herokuapp.com/back.php").then(function(tx){ 
   postMessage(tx.title); 
 });
